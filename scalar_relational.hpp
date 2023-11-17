41311c273a5e33851e5fb664d1f0be0aa33ab225
tamagobuta <146061489+tamagobuta@users.noreply.github.com>
2023-11-17 11:18:11 +0900

    /include/glm/glm/ext

--- scalar_relational.hpp
+++ scalar_relational.hpp
@@ -1,34 +1,63 @@
-/// @ref gtx_scalar_relational
-/// @file glm/gtx/scalar_relational.hpp
+/// @ref ext_scalar_relational
+/// @file glm/ext/scalar_relational.hpp
 ///
-/// @see core (dependence)
+/// @defgroup ext_scalar_relational GLM_EXT_scalar_relational
+/// @ingroup ext
 ///
-/// @defgroup gtx_scalar_relational GLM_GTX_scalar_relational
-/// @ingroup gtx
+/// Exposes comparison functions for scalar types that take a user defined epsilon values.
 ///
-/// Include <glm/gtx/scalar_relational.hpp> to use the features of this extension.
+/// Include <glm/ext/scalar_relational.hpp> to use the features of this extension.
 ///
-/// Extend a position from a source to a position at a defined length.
+/// @see core_vector_relational
+/// @see ext_vector_relational
+/// @see ext_matrix_relational
 
 #pragma once
 
-// Dependency:
-#include "../glm.hpp"
+// Dependencies
+#include "../detail/qualifier.hpp"
 
 #if GLM_MESSAGES == GLM_ENABLE && !defined(GLM_EXT_INCLUDED)
-#	ifndef GLM_ENABLE_EXPERIMENTAL
-#		pragma message("GLM: GLM_GTX_extend is an experimental extension and may change in the future. Use #define GLM_ENABLE_EXPERIMENTAL before including it, if you really want to use it.")
-#	else
-#		pragma message("GLM: GLM_GTX_extend extension included")
-#	endif
+#	pragma message("GLM: GLM_EXT_scalar_relational extension included")
 #endif
 
 namespace glm
 {
-	/// @addtogroup gtx_scalar_relational
-	/// @{
+	/// Returns the component-wise comparison of |x - y| < epsilon.
+	/// True if this expression is satisfied.
+	///
+	/// @tparam genType Floating-point or integer scalar types
+	template<typename genType>
+	GLM_FUNC_DECL GLM_CONSTEXPR bool equal(genType const& x, genType const& y, genType const& epsilon);
 
+	/// Returns the component-wise comparison of |x - y| >= epsilon.
+	/// True if this expression is not satisfied.
+	///
+	/// @tparam genType Floating-point or integer scalar types
+	template<typename genType>
+	GLM_FUNC_DECL GLM_CONSTEXPR bool notEqual(genType const& x, genType const& y, genType const& epsilon);
 
+	/// Returns the component-wise comparison between two scalars in term of ULPs.
+	/// True if this expression is satisfied.
+	///
+	/// @param x First operand.
+	/// @param y Second operand.
+	/// @param ULPs Maximum difference in ULPs between the two operators to consider them equal.
+	///
+	/// @tparam genType Floating-point or integer scalar types
+	template<typename genType>
+	GLM_FUNC_DECL GLM_CONSTEXPR bool equal(genType const& x, genType const& y, int ULPs);
+
+	/// Returns the component-wise comparison between two scalars in term of ULPs.
+	/// True if this expression is not satisfied.
+	///
+	/// @param x First operand.
+	/// @param y Second operand.
+	/// @param ULPs Maximum difference in ULPs between the two operators to consider them not equal.
+	///
+	/// @tparam genType Floating-point or integer scalar types
+	template<typename genType>
+	GLM_FUNC_DECL GLM_CONSTEXPR bool notEqual(genType const& x, genType const& y, int ULPs);
 
 	/// @}
 }//namespace glm
--- scalar_relational.inl
+++ scalar_relational.inl
@@ -1,88 +1,40 @@
-/// @ref gtx_scalar_relational
+#include "../common.hpp"
+#include "../ext/scalar_int_sized.hpp"
+#include "../ext/scalar_uint_sized.hpp"
+#include "../detail/type_float.hpp"
 
 namespace glm
 {
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool lessThan
-	(
-		T const& x,
-		T const& y
-	)
+	template<typename genType>
+	GLM_FUNC_QUALIFIER GLM_CONSTEXPR bool equal(genType const& x, genType const& y, genType const& epsilon)
 	{
-		return x < y;
+		return abs(x - y) <= epsilon;
 	}
 
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool lessThanEqual
-	(
-		T const& x,
-		T const& y
-	)
+	template<typename genType>
+	GLM_FUNC_QUALIFIER GLM_CONSTEXPR bool notEqual(genType const& x, genType const& y, genType const& epsilon)
 	{
-		return x <= y;
+		return abs(x - y) > epsilon;
 	}
 
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool greaterThan
-	(
-		T const& x,
-		T const& y
-	)
+	template<typename genType>
+	GLM_FUNC_QUALIFIER GLM_CONSTEXPR bool equal(genType const& x, genType const& y, int MaxULPs)
 	{
-		return x > y;
+		detail::float_t<genType> const a(x);
+		detail::float_t<genType> const b(y);
+
+		// Different signs means they do not match.
+		if(a.negative() != b.negative())
+			return false;
+
+		// Find the difference in ULPs.
+		typename detail::float_t<genType>::int_type const DiffULPs = abs(a.i - b.i);
+		return DiffULPs <= MaxULPs;
 	}
 
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool greaterThanEqual
-	(
-		T const& x,
-		T const& y
-	)
+	template<typename genType>
+	GLM_FUNC_QUALIFIER GLM_CONSTEXPR bool notEqual(genType const& x, genType const& y, int ULPs)
 	{
-		return x >= y;
-	}
-
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool equal
-	(
-		T const& x,
-		T const& y
-	)
-	{
-		return detail::compute_equal<T, std::numeric_limits<T>::is_iec559>::call(x, y);
-	}
-
-	template<typename T>
-	GLM_FUNC_QUALIFIER bool notEqual
-	(
-		T const& x,
-		T const& y
-	)
-	{
-		return !detail::compute_equal<T, std::numeric_limits<T>::is_iec559>::call(x, y);
-	}
-
-	GLM_FUNC_QUALIFIER bool any
-	(
-		bool const& x
-	)
-	{
-		return x;
-	}
-
-	GLM_FUNC_QUALIFIER bool all
-	(
-		bool const& x
-	)
-	{
-		return x;
-	}
-
-	GLM_FUNC_QUALIFIER bool not_
-	(
-		bool const& x
-	)
-	{
-		return !x;
+		return !equal(x, y, ULPs);
 	}
 }//namespace glm
